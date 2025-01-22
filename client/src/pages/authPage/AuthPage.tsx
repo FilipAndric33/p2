c7f135f0b9e4e30cb5470cd8422a205a3f012975
Filip Andric <andric3filip3@gmail.com>
2025-01-22 15:18:41 +0100

    Add: missing dependency in useEffect.

--- client/src/pages/authPage/AuthPage.tsx
+++ client/src/pages/authPage/AuthPage.tsx
@@ -13,7 +13,11 @@ const AuthPage: React.FC = () => {
     } else {
       return;
     }
+<<<<<<< Updated upstream
   }, []);
+=======
+  }, [location.pathname]);
+>>>>>>> Stashed changes
   return isRegister ? <RegisterForm /> : <LoginForm />;
 };
 
